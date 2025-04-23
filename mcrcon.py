5f25fd9548ae2bd18cd0ee0b44829aa0dd8e348e
peter2500zz <2797880090@qq.com>
2025-04-23 20:50:32 +0800

    制作了一个解析java json的函数

--- mcrcon.py
+++ mcrcon.py
@@ -1,7 +1,82 @@
+from unittest import case
+
 from rcon.source import Client
 import uuid
+import json
+
 
 from config import *
+
+
+def java_style_json_to_dict(java_json: str) -> dict:
+    outer = java_json[0] + java_json[-1]
+    new = java_json[1:-1]
+    # print(new)
+
+
+    if outer == "{}":
+        result = {}
+        stack = []
+        seq = ""
+        key = ""
+        value = ""
+
+        for i in new:
+            if i not in [" ", ":", ","] or stack:
+                seq += i
+
+            if i == ":" and not stack:
+                key = seq
+                seq = ""
+
+            elif stack and stack[-1] + i in ["{}", "[]", "()", "\"\""]:
+                stack.pop()
+
+                if not stack:
+                    result[key] = java_style_json_to_dict(seq)
+                    seq = ""
+
+            elif i in ["{", "[", "(", "\""]:
+
+                stack.append(i)
+
+            elif i == "," and not stack:
+                if seq:
+                    result[key] = seq
+                seq = ""
+                key = ""
+
+    elif outer == "[]":
+        result = []
+        stack = []
+        seq = ""
+
+        for i in new:
+            if i not in [" ", ","] or stack:
+                seq += i
+
+            if stack and stack[-1] + i in ["{}", "[]", "()", "\"\""]:
+                stack.pop()
+
+                if not stack:
+                    result.append(java_style_json_to_dict(seq))
+                    seq = ""
+
+            elif i in ["{", "[", "(", "\""]:
+                stack.append(i)
+
+            elif i == "," and not stack:
+                if seq:
+                    result.append(seq)
+                seq = ""
+
+    elif outer == "\"\"":
+        result = new
+
+    else:
+        result = java_json
+
+    return result
 
 
 def java_array_to_uuid(array: list[int]) -> uuid.UUID:
@@ -23,6 +98,7 @@ def test_selector(selector, client: Client):
 
 
 class MCRcon(Client):
+
     def __init__(self):
         super().__init__(f'{ipadd}', port, passwd=f'{password}')
 
@@ -41,6 +117,14 @@ class MCRcon(Client):
                 scoreboard[key] = int(value)
 
         return scoreboard
+
+    def get_data(self, target: str, arg: str) -> dict:
+        if target not in ["block", "entity", "storage"]:
+            raise Exception(f'Unknown target: {target}')
+        response = self.run(f'data get {target} {arg}')
+        response = response.split(": ", 1)[1]
+        # response = json.loads(response)
+        return (response)
 
     def get_uuid(self, selector: str) -> list[uuid.UUID]:
         response: str = self.run(f'execute as {selector} run data get entity @s UUID')
@@ -72,12 +156,21 @@ class MCRcon(Client):
 
 if __name__ == '__main__':
     with MCRcon() as mr:
-        while selector := input("请输入你的选择器: "):
-            if test_selector(selector, mr):
-                uuids = mr.get_uuid(selector)
-                print(f"匹配这个选择器实体的uuid是: {uuids}")
-            else:
-                print("无效的选择器")
+        uuids = mr.get_uuid("@e[type=pig]")
+
+        for uuid in uuids:
+            r = mr.get_data("entity", str(uuid))
+            print(r)
+            print(json.dumps(java_style_json_to_dict(r), indent=4))
+
+            break
+
+        # while selector := input("请输入你的选择器: "):
+            # if test_selector(selector, mr):
+            #     uuids = mr.get_uuid(selector)
+            #     print(f"匹配这个选择器实体的uuid是: {uuids}")
+            # else:
+            #     print("无效的选择器")
         # player_scoreboards = mr.get_scoreboards(f'Peter_2500')
         # print(player_scoreboards)
         # scoreboard_need = 'kills'
